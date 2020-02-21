baa551da6ce578cf99b4d8c06bdf6610d47a6166
hirenchalodiya1 <chalodiya.1@iitj.ac.in>
2020-02-21 05:34:15 +0530

    Solved q1 to q8

--- search/search.py
+++ search/search.py
@@ -17,7 +17,147 @@ In search.py, you will implement generic search algorithms which are called by
 Pacman agents (in searchAgents.py).
 """
 
-import util
+
+#######################################################
+#                 Cost functions                      #
+#######################################################
+
+
+def backCost(item):
+    """ item <state, actions, backwardCost> """
+    return item[2]
+
+def forwardCost(item, problem, heuristic):
+    """Find forward cost which is heuristic"""
+    
+    # heuristic None means null so return Zero
+    if heuristic == None:
+        return 0
+    
+    return heuristic(item[0], problem)
+
+def totalCost(item, problem, heuristic):
+    """sum of forward cost and backward cost"""
+    return backCost(item) + forwardCost(item, problem, heuristic)
+
+
+#######################################################
+#                 Uninformed Search                   #
+#######################################################
+
+
+def tinyMazeSearch(problem):
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+    sequence of moves will be incorrect, so only use this for tinyMaze.
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s, s, w, s, w, w, s, w]
+
+def uninformedSearch(problem, fringe):
+    # get start state
+    startState = problem.getStartState()
+    # make visited states set as graph contains cycles
+    visitedStates = []
+
+    # Initialize fringe with <start state, no actions, backward cost zero>
+    fringe.push((startState, [], 0))
+
+    while not fringe.isEmpty():
+        # cheepest node or deepest node or shallowest node
+        currentState, actions, currentCost = fringe.pop()
+
+        # if state is visited then do not search
+        if currentState in visitedStates:
+            continue
+       
+        # add state to visited state
+        visitedStates.append(currentState)
+
+        # if state is goal state then return actions
+        if problem.isGoalState(currentState):
+            return actions
+
+        # check all neighbours
+        for neighbour, action, cost in problem.getSuccessors(currentState):
+            if neighbour in visitedStates:
+                continue
+            newCost = currentCost + cost
+            newAction = actions + [action]
+            fringe.push((neighbour, newAction, newCost))
+
+    #  if not path found then return empty set
+    # return []
+    # raise path not found for debug purpose
+    raise ValueError("path not found")
+
+def depthFirstSearch(problem):
+    """
+    Search the deepest nodes in the search tree first.
+
+    Your search algorithm needs to return a list of actions that reaches the
+    goal. Make sure to implement a graph search algorithm.
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    """
+    "*** YOUR CODE HERE ***"
+
+    from util import Stack
+    
+    fringe = Stack()
+    return uninformedSearch(problem, fringe)
+
+def breadthFirstSearch(problem):
+    """Search the shallowest nodes in the search tree first."""
+    "*** YOUR CODE HERE ***"
+    from util import Queue
+
+    fringe = Queue()
+    return uninformedSearch(problem, fringe)
+
+def uniformCostSearch(problem):
+    """Search the node of least total cost first."""
+    "*** YOUR CODE HERE ***"
+    from util import PriorityQueueWithFunction
+    
+    fringe = PriorityQueueWithFunction(backCost)
+    return uninformedSearch(problem, fringe)
+
+def greedySearch(problem, heuristic=None):
+    """Search the node that has the least heuristic cost."""
+    "*** YOUR CODE HERE ***"
+    from util import PriorityQueueWithFunction
+    
+    fringe = PriorityQueueWithFunction(lambda x: forwardCost(x, problem=problem, heuristic=heuristic))
+    return uninformedSearch(problem, fringe)
+
+def aStarSearch(problem, heuristic=None):
+    """Search the node that has the lowest combined cost and heuristic first."""
+    "*** YOUR CODE HERE ***"
+    from util import PriorityQueueWithFunction
+    
+    fringe = PriorityQueueWithFunction(lambda x: totalCost(x, problem=problem, heuristic=heuristic))
+    return uninformedSearch(problem, fringe)
+
+# Abbreviations
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+ucs = uniformCostSearch
+greedy = greedySearch
+
+
+#######################################################
+# below changes are made so that autograder can run   #
+#######################################################
+
 
 class SearchProblem:
     """
@@ -60,60 +200,3 @@ class SearchProblem:
         The sequence must be composed of legal moves.
         """
         util.raiseNotDefined()
-
-
-def tinyMazeSearch(problem):
-    """
-    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
-    sequence of moves will be incorrect, so only use this for tinyMaze.
-    """
-    from game import Directions
-    s = Directions.SOUTH
-    w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
-
-def depthFirstSearch(problem):
-    """
-    Search the deepest nodes in the search tree first.
-
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
-
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
-
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
-    """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
-
-def breadthFirstSearch(problem):
-    """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
-
-def uniformCostSearch(problem):
-    """Search the node of least total cost first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
-
-def nullHeuristic(state, problem=None):
-    """
-    A heuristic function estimates the cost from the current state to the nearest
-    goal in the provided SearchProblem.  This heuristic is trivial.
-    """
-    return 0
-
-def aStarSearch(problem, heuristic=nullHeuristic):
-    """Search the node that has the lowest combined cost and heuristic first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
-
-
-# Abbreviations
-bfs = breadthFirstSearch
-dfs = depthFirstSearch
-astar = aStarSearch
-ucs = uniformCostSearch
